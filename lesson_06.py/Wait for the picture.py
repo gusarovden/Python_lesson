b94494ccc144bc84fa7afa62a57df486b1b2c47e
gusarovden <gusarovden1@gmail.com>
2025-12-28 16:06:31 +0300

    https://github.com/gusarovden/Python_lesson.git

--- lesson_06.py/Wait for the picture.py
+++ lesson_06.py/Wait for the picture.py
@@ -7,12 +7,12 @@ driver = webdriver.Chrome()
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-wait = WebDriverWait(driver, 15)
+wait = WebDriverWait(driver, 30)
 wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape"))
+    EC.presence_of_element_located((By.XPATH, "(//img)[5]"))
 )
     
-third_img = driver.find_element(By.CSS_SELECTOR, "#award")
+third_img = driver.find_element(By.XPATH, "(//img)[4]")
 src_value = third_img.get_attribute("src")
    
 print(src_value)
